ccabc634eb986249e127df141e190787be12622f
Soham Kayal <sohamkayal@Sohams-Laptop.local>
2024-05-20 18:26:21 +0530

    Refactored into model, view and controller
    Signed-off-by: Soham Kayal <sohamkayal@Sohams-Laptop.local>

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var timer = Timer();
+    var quizBrain = QuizBrain();
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI();
+    }
+    
+    @IBAction func answerButtonPresed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!;
+        if(quizBrain.checkAnswer(userAnswer)){
+            print("User Answered correctly");
+            sender.backgroundColor = UIColor.green;
+            quizBrain.nextQuestion(true);
+        }
+        else{
+            print("User Answered Incorrectly");
+            sender.backgroundColor = UIColor.red;
+            quizBrain.nextQuestion(false);
+        }
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self,   selector: (#selector(ViewController.updateUI)), userInfo: nil, repeats: false)
+        //updateUI();
+        
+    }
+    
+    @objc func updateUI() {
+        let progressFraction : Float = quizBrain.getProgress();
+        progressBar.setProgress(progressFraction, animated: true);
+        questionLabel.text = quizBrain.getQuestionText();
+        scoreLabel.text = "Score: \(quizBrain.getScore())";
+        trueButton.backgroundColor = UIColor.clear;
+        falseButton.backgroundColor = UIColor.clear;
+    }
+    
+    
+}
+
